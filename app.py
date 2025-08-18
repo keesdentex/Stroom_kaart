eb65f967cc933f90a848240468aa01f9f6f83039
keesdentex <167865764+keesdentex@users.noreply.github.com>
2025-08-18 11:17:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import streamlit as st
 st.title("Mijn Bereken App")
 
 # Inputvelden
-a = st.number_input("Voer het eerste getal in", value=0.0)
-b = st.number_input("Voer het tweede getal in", value=0.0)
+stroom_links = st.number_input("Stroomsnelheid links", value=0.0)
+stroom_rechts = st.number_input("Stroomsnelheid rechts", value=0.0)
+stroom_hoek = st.number_input("Hoek van stroom ten opzichte van de wind (rechts positief, links negatief)", value=0.0)
+wind_snelheid= st.number_input("Windsnelheid in knopen", value=0.0)
 
 # Actieknop
 if st.button("Bereken"):
     resultaat = a + b  # Hier komt jouw berekening
     st.success(f"Het resultaat is: {resultaat}")
+
